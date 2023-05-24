ac2de4a7fe96aa0c46a1b315666b93ad580620a7
Jason Inaba <jasonrinaba@g.ucla.edu>
2023-05-24 16:35:51 -0700

    handle client

--- project/main.cpp
+++ project/main.cpp
@@ -13,37 +13,8 @@ using namespace std;
 const int BUFFER_SIZE = 8192;
 
 void handleClient(int clientSocket) {
-  // char buffer[BUFFER_SIZE];
-
-  // ssize_t bytesRead = recv(clientSocket, buffer, BUFFER_SIZE, 0);
-  // if (bytesRead > 0) 
-  // {
-  //   std::string request(buffer, bytesRead);
-    
-  //   std::stringstream ss;
-  //   ss << std::hex << std::setfill('0');
-  //   for (char c : request) {
-  //       ss << std::setw(2) << static_cast<int>(static_cast<unsigned char>(c)) << " ";
-  //   }
-  //   std::string hexRequest = ss.str();
-  //   std::cout << "Received request in hex: \n" << hexRequest << std::endl;
-
-    
-    // std::string responseBody = "<html><body><h1>Hello from server!</h1></body></html>";
-
-    // std::string response = "HTTP/1.1 200 OK\r\n";
-    // response += "Content-Type: text/html\r\n";
-    // response += "Content-Length: " + std::to_string(responseBody.length()) + "\r\n";
-    // response += "\r\n";
-    // response += responseBody;
-
-    // ssize_t bytesSent = send(clientSocket, response.c_str(), response.length(), 0);
-    // if (bytesSent < 0) {
-    //     std::cerr << "Error sending response to client" << std::endl;
-    // }
-  // }
-
   char buffer[BUFFER_SIZE];
+  Datagram datagram;
 
   ssize_t bytesRead = recv(clientSocket, buffer, BUFFER_SIZE, 0);
   if (bytesRead > 0) {
@@ -56,8 +27,11 @@ void handleClient(int clientSocket) {
       std::string hexRequest = ss.str();
       std::cout << "Received request in hex: \n" << hexRequest << std::endl;
 
-      Datagram datagram = parseIPDatagram(hexRequest);
+      datagram = parseIPDatagram(hexRequest);
   }
+
+  // forward packet locally
+  
 
   close(clientSocket);
 }
