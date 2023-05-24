7eadc8796f2d20b775b4ac66aba9111e52f39503
Jason Inaba <jasonrinaba@g.ucla.edu>
2023-05-23 17:15:01 -0700

    fix config parser

--- project/config_parser.cpp
+++ project/config_parser.cpp
@@ -1,6 +1,6 @@
 #include "config_parser.h"
 
-ConfigParser::ConfigParser(istream& in) : in(in) {}
+ConfigParser::ConfigParser(string in) : in(in) {}
 
 IpConfig ConfigParser::getIpConfig() const{
     return ipConfig;
@@ -17,33 +17,41 @@ void ConfigParser::parseIpConfig(){
     /*
     Parse the IP configuration file.
     The first line is the router's LAN IP and the WAN IP.
+    The line containing "0.0.0.0" is the WAN port.
     The following lines are the client's IPs.
     */
+    istringstream iss(in);  // create istringstream from in
+
     string line;
-    getline(in, line);
-    stringstream ss(line);
-    string lanIp, wanIp;
-    ss >> lanIp >> wanIp;
-    ipConfig.lanIP = lanIp;
-    ipConfig.wanIP = wanIp;
+    if (getline(iss, line)) {
+        stringstream ss(line);  // create stringstream from line
+        string lanIp, wanIp;
+        ss >> lanIp >> wanIp;  // extract values from stringstream
+        ipConfig.lanIP = lanIp;
+        ipConfig.wanIP = wanIp;
+    }
 
-    // skip a line
-    getline(in, line); 
-
-    while(getline(in, line)) {
+    // print the result of the getline fcn
+    while(getline(iss, line)) {
+        // skip line if it contains "0.0.0.0"
+        if(line == "0.0.0.0") continue;
         // break out on new lines
         if(line == "") break;
         ipConfig.clientIps.push_back(line);
     }
-}
 
+    // move the ConfigParser.in pointer to the next line
+    in = iss.str().substr(iss.tellg());
+}
 void ConfigParser::parseNaptConfig(){
     /*
     Parse the NAPT configuration file.
     Each line is a mapping from a LAN IP and port to a WAN port.
     */
+    istringstream iss(in);  // create istringstream from in
+
     string line;
-    while(getline(in, line)) {
+    while(getline(iss, line)) {
         // break out on new lines
         if(line.empty()) break;
 
--- project/main.cpp
+++ project/main.cpp
@@ -37,8 +37,59 @@ void handleClient(int clientSocket) {
 }
 
 
-int main() {
 
+
+
+
+
+// int main() {
+//   std::string szLine;
+
+//   // First line is the router's LAN IP and the WAN IP
+//   std::getline(std::cin, szLine);
+//   size_t dwPos = szLine.find(' ');
+//   auto szLanIp = szLine.substr(0, dwPos);
+//   auto szWanIp = szLine.substr(dwPos + 1);
+
+//   std::cout << "Server's LAN IP: " << szLanIp << std::endl
+//             << "Server's WAN IP: " << szWanIp << std::endl;
+
+//   // TODO: Modify/Add/Delete files under the project folder.
+
+//   return 0;
+// }
+
+
+
+int main() {
+  // Parse the configuration files
+  string configInput;
+  string temp;
+  while (getline(cin, temp))
+  {
+      configInput += temp + "\n";
+  }
+  
+  ConfigParser parser(configInput);
+  parser.parse();
+
+  auto ipConfig = parser.getIpConfig();
+  auto naptConfig = parser.getNaptConfig();
+
+  // print ipConfig
+  std::cout << "Router IP: " << ipConfig.lanIP << std::endl;
+  std::cout << "Router WAN IP: " << ipConfig.wanIP << std::endl;
+  for (const auto& clientIp : ipConfig.clientIps) {
+      std::cout << "Client IP: " << clientIp << std::endl;
+  }
+
+  // print naptConfig
+  for (const auto& [lanIp, entry] : naptConfig.lanToWanMap) {
+      std::cout << "NAPT Entry: " << lanIp.first << ", " << lanIp.second << ", " << entry << std::endl;
+  }
+
+
+  // ======================
   int serverSocket;
   struct sockaddr_in serverAddress;
   int port = 5152; // Replace with your desired server port
@@ -75,64 +126,23 @@ int main() {
 
     int clientSocket = accept(serverSocket, reinterpret_cast<struct sockaddr*>(&clientAddress), &clientAddressLength);
     if (clientSocket < 0) {
-      std::cerr << "Error accepting client connection" << std::endl;
+      perror("Error accepting client connection");
       continue;
     }
-    std::cout << "New client connected" << std::endl;
+    std::cout << "New client connected" << clientSocket << std::endl;
     handleClient(clientSocket);
 
   }
 
-
-
-  std::istringstream configInput(
-        "192.168.1.1 98.149.235.132\n"
-        "0.0.0.0\n"
-        "192.168.1.100\n"
-        "192.168.1.200\n"
-        "\n"
-        "192.168.1.100 8080 8080\n"
-        "192.168.1.200 9000 443\n"
-        "\n"
-    );
-
-  ConfigParser parser(configInput);
-  parser.parse();
-
-  auto ipConfig = parser.getIpConfig();
-  auto naptConfig = parser.getNaptConfig();
-
-  // print ipConfig
-  std::cout << "Router IP: " << ipConfig.lanIP << std::endl;
-  std::cout << "Router WAN IP: " << ipConfig.wanIP << std::endl;
-  for (const auto& clientIp : ipConfig.clientIps) {
-      std::cout << "Client IP: " << clientIp << std::endl;
-  }
-
-  // print naptConfig
-  for (const auto& [lanIp, entry] : naptConfig.lanToWanMap) {
-      std::cout << "NAPT Entry: " << lanIp.first << ", " << lanIp.second << ", " << entry << std::endl;
-  }
+  // std::istringstream configInput(
+  //       "192.168.1.1 98.149.235.132\n"
+  //       "0.0.0.0\n"
+  //       "192.168.1.100\n"
+  //       "192.168.1.200\n"
+  //       "\n"
+  //       "192.168.1.100 8080 8080\n"
+  //       "192.168.1.200 9000 443\n"
+  //       "\n"
+  //   );
 }
-
-
-
-
-// int main() {
-//   std::string szLine;
-
-//   // First line is the router's LAN IP and the WAN IP
-//   std::getline(std::cin, szLine);
-//   size_t dwPos = szLine.find(' ');
-//   auto szLanIp = szLine.substr(0, dwPos);
-//   auto szWanIp = szLine.substr(dwPos + 1);
-
-//   std::cout << "Server's LAN IP: " << szLanIp << std::endl
-//             << "Server's WAN IP: " << szWanIp << std::endl;
-
-//   // TODO: Modify/Add/Delete files under the project folder.
-
-//   return 0;
-// }
-
 
